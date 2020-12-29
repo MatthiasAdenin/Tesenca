300bc1c38779d96c93bd70b29ac82883855bfd07
Matthias <matthiasadenin@gmail.com>
2020-12-29 22:13:30 +0100

    feat : Add endpoint api to render companies

--- src/main/java/com/matthias/adenin/tesenca/controller/TesencaController.java
+++ src/main/java/com/matthias/adenin/tesenca/controller/TesencaController.java
@@ -2,6 +2,8 @@ package com.matthias.adenin.tesenca.controller;
 
 import com.matthias.adenin.tesenca.model.CompanyDto;
 import com.matthias.adenin.tesenca.repository.CompanyRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -9,22 +11,25 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@CrossOrigin
 public class TesencaController {
+
+    Logger LOGGER = LoggerFactory.getLogger(TesencaController.class);
 
     @Autowired
     CompanyRepository companyRepository;
 
     @PutMapping("/saveCompany")
-    @CrossOrigin
     public CompanyDto saveCompany(@RequestBody CompanyDto companyDto) {
         return companyRepository.save(companyDto);
     }
 
     @GetMapping("/getCompanies")
-    @CrossOrigin
     public List<CompanyDto> getCompanies() {
+        LOGGER.info("START - Retrieve getCompanies");
         List<CompanyDto> companyDtoList = new ArrayList<>();
         companyRepository.findAll().forEach(companyDtoList::add);
+        LOGGER.info("END - Retrieve getCompanies");
         return companyDtoList;
     }
 
